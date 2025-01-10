6aec8c2e5aa5b3d1174546641a9cbd5bdf8cdc8b
ronitdalal27 <dalalronit131@gmail.com>
2025-01-10 12:53:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
-// your code here
+	// your code here
+	if (num < 0) return "Invalid input"; // Handle negative input
+    if (num === 0) return 0; // Base case: the 0th Fibonacci number is 0
+    if (num === 1) return 1; // Base case: the 1st Fibonacci number is 1
+    
+    let a = 0;
+    let b = 1;
+    let fib;
+
+    for (let i = 2; i <= num; i++) {
+        fib = a + b;
+        a = b;
+        b = fib;
+    }
+
+    return fib;
 }
 
 module.exports = fibonacci;
