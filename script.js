1ad5b7bd9237d975ed9198eb1c018edb542f450c
ronitdalal27 <dalalronit131@gmail.com>
2025-01-10 12:59:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 function fibonacci(num) {
 	// your code here
-    if (num === 0) return 0; // Base case: the 0th Fibonacci number is 0
-    if (num === 1) return 1; // Base case: the 1st Fibonacci number is 1
+    if (num === 0) return 0; // Base case
+    if (num === 1) return 1; // Base case
     
-    let a = 0;
-    let b = 1;
-    let fib;
-
-    for (let i = 2; i <= num; i++) {
-        fib = a + b;
-        a = b;
-        b = fib;
+    let a = 0, b = 1;
+    for (let i = 2; i < num; i++) { // Loop till num-1 for 0-based index
+        [a, b] = [b, a + b]; // Update a and b using array destructuring
     }
-
-    return fib;
+    return b;
 }
 
 module.exports = fibonacci;
